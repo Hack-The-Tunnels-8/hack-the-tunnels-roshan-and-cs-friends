0cf6fd62ccfc994c4921624384f6d4932a14e7fb
Ethan Huynh <71572001+eggrollius@users.noreply.github.com>
2023-09-30 18:13:36 +0000

    testing push pull for saad

--- service/src/infrastructure/api/router.ts
+++ service/src/infrastructure/api/router.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const getAPIRoot = async (_: Request, response: Response) => {
   response.json({
-    message: "Hello World!!!",
+    message: "Hello World",
   });
 };
 
